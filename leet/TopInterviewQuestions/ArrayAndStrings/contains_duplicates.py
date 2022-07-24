ce61da5f6e37cf8b1d5fc6e64702eb32383a509f
jreyesco16 <jesserc.2@gmail.com>
2022-07-23 23:55:19 -0500

    solution for contains duplicate

---
+++ leet/TopInterviewQuestions/ArrayAndStrings/contains_duplicates.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        
+        s = set(nums)
+
+        if len(nums) == len(s):
+            return False
+        
+        return True
+    
+
+
+def main():
+    test_case = [1, 2, 3, 1]
+    expected = True
+
+    solution = Solution()
+
+    result = solution.containsDuplicate(test_case)
+
+    if expected == result:
+        print("PASSED")
+    else:
+        print("FAILED")
+
+
+if __name__ == "__main__":
+    main()
