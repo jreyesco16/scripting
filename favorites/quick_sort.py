a5d548fa4d5b2667f4550876db857d6f093b2c1e
jreyesco16 <jesserc.2@gmail.com>
2023-01-26 00:32:34 -0600

    updated quicksort function to be functional

--- favorites/quick_sort.py
+++ favorites/quick_sort.py
@@ -1,21 +1,55 @@
 from typing import List
+import random
 
-def QuickSort(nums: List[int]) -> List[int]:
+def Swap(nums: List[int], l: int, r: int):
+    tmp = nums[l]
+    nums[l] = nums[r]
+    nums[r] = tmp
 
+def QuickSort(nums: List[int], l: int, r: int):
+    # base case
+    if r <= l:
+        return
+
+    oL, oR = l, r
     
+    p = r # pivot: nums[p]
+    r = p-1 
 
-    return nums
+
+    while l < r:
+        while nums[l] < nums[p] and l < r:
+            l+=1
+    
+        while nums[p] < nums[r] and l < r:
+            r-=1
+
+        Swap(nums, l, r)
+        if l < r:
+            l+=1
+            r-=1
+
+    # edge case: if pivot is larger than right pointer than move right up by one
+    if nums[r] < nums[p]:
+        r+=1
+
+    Swap(nums, r, p)
+
+    QuickSort(nums, oL, r-1) # left partition
+    QuickSort(nums, r+1, oR) # right partition
+
 
 def main():
+    nums = random.sample(range(-100, 100), 100)
 
-    nums = [2, 29, 42, 298, 223, 2, 43, 492, 23, 94, 10, 2232, 223, 902, 23, 43]
+    sorted_nums = QuickSort(nums, 0, len(nums)-1)
 
-    sorted_nums = QuickSort(nums)
-
-    if nums.sort() == sorted_nums:
-        print("PASSED")
-    else:
+    if nums.sort() != sorted_nums:
         print("FAILED")
+        return
 
 if __name__=="__main__":
-    main()
+    for i in range(100):
+        main()
+
+    print("PASSED")
